f9d14daa508015485ed66759d466b4ba29b663ab
Wangyananblink <yanan.20@intl.zju.edu.cn>
2021-03-11 16:16:31 +0800

    11/03

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,18 @@
+a = 1
+b = 1
+i = 1
+# make the code run for 11 times
+for i in range (1,12):
+# distinguish  two situations
+        if i % 2 == 1:
+            a = a + b
+            print(a)
+        else:
+            b = a + b
+            print(b)
+            
+
+           
+
+
+
